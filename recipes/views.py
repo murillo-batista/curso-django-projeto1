7c86b38997f98981c7d53948138c3a302c062d80
Murillo Batista dos Santos <murillo.suprir@gmail.com>
2025-01-07 16:47:04 -0300

    RenderTemplate

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("HOME 2")
+    return render(request, "home.html")
 
 
 def contato(request):
